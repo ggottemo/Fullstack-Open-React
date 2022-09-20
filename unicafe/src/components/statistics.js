5cc0130faa9933793770b31d1d8c73d45c32bd9a
George Gottemoeller <George.Gottemoeller@franklincountyohio.gov>
2022-09-20 15:11:21 -0400

    1.11 HTML Table

--- unicafe/src/components/statistics.js
+++ unicafe/src/components/statistics.js
@@ -1,17 +1,27 @@
 import React from "react";
-
+import StatisticLine from "./statisticsline";
 const Statistics = ({ state }) => {
-
-    return (
-        <div >
-        <p>good {state.good}</p>
-        <p>neutral {state.neutral}</p>
-        <p>bad {state.bad}</p>
-        <p>all {state.good + state.neutral + state.bad}</p>
-        <p>average { ((state.good - state.bad) / (state.good + state.neutral + state.bad)).toFixed(2)}</p>
-        <p>positive {((state.good / (state.good + state.neutral + state.bad) * 100)).toFixed(2)}%</p>
-        </div>
-    );
+    if (state.good === 0 && state.neutral === 0 && state.bad === 0) {
+        return (
+            <div>
+            <p>No feedback given</p>
+            </div>
+        );
     }
+    else {
+
+        return (
+            <table >
+            <StatisticLine text="good"  value={state.good} />
+            <StatisticLine text="neutral" value={state.neutral} />
+            <StatisticLine text="bad" value={state.bad} />
+            <StatisticLine text="all" value={state.good + state.neutral + state.bad} />
+            <StatisticLine text="average" value={ ((state.good - state.bad) / (state.good + state.neutral + state.bad)).toFixed(2)} />
+            <StatisticLine text="positive" value={((state.good / (state.good + state.neutral + state.bad) * 100)).toFixed(2) } />
+            </table>
+        );
+       
+    }
+}
 
 export default Statistics;
---
+++ unicafe/src/components/statisticsline.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const StatisticLine = ({ text, value }) => {
+    return (
+        <tr>
+        <td>{text}</td>
+        <td>{value} {text == "positive" ? "%" : ""}</td>
+        </tr>
+    );
+    }
+
+
+export default StatisticLine;
