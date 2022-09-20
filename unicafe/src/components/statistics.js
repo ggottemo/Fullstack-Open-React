8250eee28a57c0e2e4512c57691ae8d3d8df2960
George Gottemoeller <George.Gottemoeller@franklincountyohio.gov>
2022-09-20 14:55:03 -0400

    1.7

--- unicafe/src/components/statistics.js
+++ unicafe/src/components/statistics.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Statistics = ({ state }) => {
+
     return (
         <div >
         <p>good {state.good}</p>
         <p>neutral {state.neutral}</p>
         <p>bad {state.bad}</p>
+        <p>all {state.good + state.neutral + state.bad}</p>
+        <p>average { ((state.good - state.bad) / (state.good + state.neutral + state.bad)).toFixed(2)}</p>
+        <p>positive {((state.good / (state.good + state.neutral + state.bad) * 100)).toFixed(2)}%</p>
         </div>
     );
     }
