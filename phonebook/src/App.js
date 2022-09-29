ffc443bc9abb9ba37c7eb2d1a771f49ae02a2d4d
George Gottemoeller <George.Gottemoeller@franklincountyohio.gov>
2022-09-29 14:41:39 -0400

    Exercise 2.7

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,38 +1,43 @@
-import { useState } from 'react'
-import { Contact_list } from './components/contact'
+import { useState } from "react";
+import { Contact_list } from "./components/contact";
 const App = () => {
-  
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
-    
-  ]) 
-  const [newName, setNewName] = useState('Enter Name')
-  const [current_id, setCurrent_id] = useState(1)
-
+  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("Enter Name");
+  const [current_id, setCurrent_id] = useState(1);
+  // Handlers
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    setCurrent_id(current_id + 1);
+    return setPersons(persons.concat([{ name: newName, id: current_id + 1 }]));
+  };
   return (
-    
     <div>
-      
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={(event)=> {
-            event.preventDefault()
-            return setNewName(event.target.value)
-          }}/>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => {
+              event.preventDefault();
+              return setNewName(event.target.value);
+            }}
+          />
         </div>
         <div>
-          <button type="submit" onClick={ (event) => {
-            event.preventDefault()
-            setCurrent_id(current_id + 1)
-            return setPersons(persons.concat([{ name: newName, id: current_id+1 }]))
-          }}>add</button>
+          <button type="submit" onClick={handleAddUser}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
       <Contact_list contacts={persons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- phonebook/src/components/contact.js
+++ phonebook/src/components/contact.js
@@ -1,25 +1,23 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-export const Contact = ({name}) => {
-    return (
-        <p>{name} </p>
-    )
-}
+import PropTypes from "prop-types";
+import React from "react";
+export const Contact = ({ name }) => {
+  return <p>{name} </p>;
+};
 Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-
-}
+  name: PropTypes.string.isRequired,
+};
 
 // Contacts
 
-export const Contact_list = ({contacts}) => {
-    return (
-        <div>
-            {contacts.map(contact => <Contact key={contact.id} name={contact.name}  />)}
-        </div>
-        
-    )
-}
+export const Contact_list = ({ contacts }) => {
+  return (
+    <div>
+      {contacts.map((contact) => (
+        <Contact key={contact.id} name={contact.name} />
+      ))}
+    </div>
+  );
+};
 Contact_list.propTypes = {
-    contacts: PropTypes.array.isRequired
-}
+  contacts: PropTypes.array.isRequired,
+};
