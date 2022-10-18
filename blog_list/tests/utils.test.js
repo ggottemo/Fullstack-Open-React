fef056abb850852c110e6f6df378dc7a84268b67
George Gottemoeller <George.Gottemoeller@franklincountyohio.gov>
2022-10-18 15:55:33 -0400

    Excercise 4.5 Helper Functions

--- blog_list/tests/utils.test.js
+++ blog_list/tests/utils.test.js
@@ -1,4 +1,6 @@
-const { dummy, totalLikes } = (await import("../utils/list_helper.js")).default;
+const { dummy, totalLikes, favoriteBlog } = (
+  await import("../utils/list_helper.js")
+).default;
 debugger;
 describe("Util Functions", () => {
   test("dummy returns one", () => {
@@ -7,58 +9,110 @@ describe("Util Functions", () => {
     expect(result).toBe(1);
   });
   ////////////////////////////////////////
-  test("total likes", () => {
-    const blogs = [
-      {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        likes: 7,
-      },
-      {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        likes: 5,
-      },
-      {
+  describe("total likes", () => {
+    test("total likes", () => {
+      const blogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          likes: 7,
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          likes: 5,
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          likes: 12,
+        },
+        {
+          _id: "5a422b891b54a676234d17fa",
+          title: "First class tests",
+          likes: 10,
+        },
+        {
+          _id: "5a422ba71b54a676234d17fb",
+          title: "TDD harms architecture",
+          likes: 0,
+        },
+        {
+          _id: "5a422bc61b54a676234d17fc",
+          title: "Type wars",
+          likes: 2,
+        },
+      ];
+      const result = totalLikes(blogs);
+      expect(result).toBe(36);
+    });
+    ////////////////////////////////////////////
+    test("likes with 0 blogs", () => {
+      const blogs = [];
+      const result = totalLikes(blogs);
+      expect(result).toBe(0);
+    });
+    ////////////////////////////////////////////
+    test("likes with 1 blog", () => {
+      const blogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          likes: 7,
+        },
+      ];
+      const result = totalLikes(blogs);
+      expect(result).toBe(7);
+    });
+  });
+
+  describe("favorite blog", () => {
+    test("favorite blog", () => {
+      const blogs = [
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          likes: 5,
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          likes: 12,
+        },
+        {
+          _id: "5a422b891b54a676234d17fa",
+          title: "First class tests",
+          likes: 10,
+        },
+      ];
+      const result = favoriteBlog(blogs);
+      expect(result).toEqual({
         _id: "5a422b3a1b54a676234d17f9",
         title: "Canonical string reduction",
         likes: 12,
-      },
-      {
-        _id: "5a422b891b54a676234d17fa",
-        title: "First class tests",
-        likes: 10,
-      },
-      {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
-        likes: 0,
-      },
-      {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        likes: 2,
-      },
-    ];
-    const result = totalLikes(blogs);
-    expect(result).toBe(36);
-  });
-  ////////////////////////////////////////////
-  test("likes with 0 blogs", () => {
-    const blogs = [];
-    const result = totalLikes(blogs);
-    expect(result).toBe(0);
-  });
-  ////////////////////////////////////////////
-  test("likes with 1 blog", () => {
-    const blogs = [
-      {
+      });
+    });
+    ////////////////////////////////////////////
+    test("favorite blog with 0 blogs", () => {
+      const blogs = [];
+      const result = favoriteBlog(blogs);
+      expect(result).toEqual({});
+    });
+    ////////////////////////////////////////////
+    test("favorite blog with 1 blog", () => {
+      const blogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          likes: 7,
+        },
+      ];
+      const result = favoriteBlog(blogs);
+      expect(result).toEqual({
         _id: "5a422a851b54a676234d17f7",
         title: "React patterns",
         likes: 7,
-      },
-    ];
-    const result = totalLikes(blogs);
-    expect(result).toBe(7);
+      });
+    });
   });
 });
--- blog_list/utils/list_helper.js
+++ blog_list/utils/list_helper.js
@@ -6,4 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-export default { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  const favorite = blogs.reduce((favorite, blog) => {
+    if (favorite.likes < blog.likes) {
+      return blog;
+    }
+    return favorite;
+  }, blogs[0]);
+  return favorite;
+};
+
+export default { dummy, totalLikes, favoriteBlog };
