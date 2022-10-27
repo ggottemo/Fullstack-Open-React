afbd6840bacd1957b74f48c77bdcdbc69b26998d
George Gottemoeller <George.Gottemoeller@franklincountyohio.gov>
2022-10-27 08:43:48 -0400

    Login.js

--- bloglist-frontend/src/components/Login.js
+++ bloglist-frontend/src/components/Login.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = ({ handleLogin, username, password }) => {
+const Login = ({ handleLogin }) => {
+  // States
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
       <h2>Log in to application</h2>
       <form onSubmit={handleLogin}>
         <div>
           username
-          <input reset={null} />
+          <input
+            value={username}
+            reset={null}
+            onChange={(val) => setUsername(val)}
+          />
         </div>
         <div>
           password
-          <input reset={null} />
+          <input
+            value={password}
+            reset={null}
+            onChange={(val) => setPassword(val)}
+          />
         </div>
         <button type="submit">login</button>
       </form>
     </div>
   );
 };
+
+export default Login;
