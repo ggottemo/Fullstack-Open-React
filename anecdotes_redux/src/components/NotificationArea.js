11b1ffa866798ce51ba9aca4c44f1f12d45e4104
George Gottemoeller <George.Gottemoeller@franklincountyohio.gov>
2022-11-03 09:36:22 -0400

    6.19 Modify Notification to use connect

--- anecdotes_redux/src/components/NotificationArea.js
+++ anecdotes_redux/src/components/NotificationArea.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 import Notification from "./Notification";
 
-const NotificationArea = () => {
-  const notifications = useSelector((state) => state.notifications);
-
-  return notifications.map((notification) => (
+const NotificationArea = (props) => {
+  return props.notifications.map((notification) => (
     <Notification key={notification.id} message={notification.message} />
   ));
 };
 
-export default NotificationArea;
+const mapStateToProps = (state) => {
+  return {
+    notifications: state.notifications,
+  };
+};
+
+const ConnectedNotificationArea = connect(mapStateToProps)(NotificationArea);
+export default ConnectedNotificationArea;
