cd44d74296881bfbef7ca3eca4c7d9da97bedf80
ggottemo <ggottemo717@gmail.com>
2022-11-08 16:54:33 -0500

    Add useAnecdotes hook to prepare for global constants

--- webpack_example/src/app.js
+++ webpack_example/src/app.js
@@ -1,8 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+const useAnecdotes = (url) => {
+    const [anecdotes, setAnecdotes] = useState([])
 
+    useEffect(  () => {
+        (async () => {
+            const response = await axios.get(url)
+            setAnecdotes(response.data)
+
+        })()
+        }, [url])
+
+
+    return anecdotes
+
+}
 const App = () => {
     const [counter, setCounter] = useState(0)
     const [values, setValues] = useState([])
+    const url = 'http://localhost:3001/anecdotes'
+    const anecdotes = useAnecdotes(url)
+
     const handleClick = () => {
         setCounter(counter + 1)
         setValues(values.concat(counter))
@@ -11,6 +29,10 @@ const App = () => {
         <div className={"container"}>
             hello webpack {counter}
             <button onClick={handleClick}>plus</button>
+            <div> {anecdotes.length} anecdotes on the server</div>
+            <ul>
+                {anecdotes.map(anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
+            </ul>
         </div>
     )
 }
