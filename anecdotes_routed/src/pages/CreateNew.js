12ebd2761f11687e1e805d2eb9b8f7598fd90956
ggottemo <ggottemo717@gmail.com>
2022-11-08 09:33:35 -0500

    7.4 & 7.5 useField

--- anecdotes_routed/src/pages/CreateNew.js
+++ anecdotes_routed/src/pages/CreateNew.js
@@ -1,61 +1,66 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useField} from "../hooks/index.js";
 
-const CreateNew = ({ addNew, notify }) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+const CreateNew = ({addNew, notify}) => {
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("");
+    const [info, setInfo] = useState("");
 
-  // hook to redirect back home after creation
-  const navigate = useNavigate();
+    // hook to redirect back home after creation
+    const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addNew({
-      content,
-      author,
-      info,
-      votes: 0,
-    });
-    navigate("/");
-    notify(`Anecdote created: ${content}`);
-    setTimeout(() => {
-      notify("");
-    }, 5000);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addNew({
+            content,
+            author,
+            info,
+            votes: 0,
+        });
+        navigate("/");
+        notify(`Anecdote created: ${content}`);
+        setTimeout(() => {
+            notify("");
+        }, 5000);
+    };
 
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+    // set up hooks for each field
+    const contentHook = useField("text");
+    const authorHook = useField("text");
+    const infoHook = useField("text");
+
+    return (
         <div>
-          content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+            <h2>create a new anecdote</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    content
+                    <input
+                        {...contentHook}
+                    />
+                </div>
+                <div>
+                    author
+                    <input
+                        {...authorHook}
+                    />
+                </div>
+                <div>
+                    url for more info
+                    <input
+                        {...infoHook}
+                    />
+                </div>
+                <button>create</button>
+                <button type={"button"} onClick={()=>{
+                    contentHook.reset();
+                    authorHook.reset();
+                    infoHook.reset();
+                }}>reset</button>
+            </form>
         </div>
-        <div>
-          author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-        </div>
-        <div>
-          url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default CreateNew;
