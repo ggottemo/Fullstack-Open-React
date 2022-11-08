eeebafefed4d4ba21691a8bb7bbfb11c76758cf5
ggottemo <ggottemo717@gmail.com>
2022-11-08 15:32:59 -0500

    Add babel preset env transpiler s

--- webpack_example/src/index.js
+++ webpack_example/src/index.js
@@ -1,5 +1,7 @@
 import App from './App'
 import React from 'react'
+import 'core-js/stable/index.js'
+import 'regnerator-runtime/runtime.js'
 import ReactDOM from 'react-dom'
 
 const hello = name => {
