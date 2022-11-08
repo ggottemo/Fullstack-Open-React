fdc3004ff6f2738145a93dc4798735a780094748
ggottemo <ggottemo717@gmail.com>
2022-11-08 16:34:57 -0500

    Source Maps

--- webpack_example/src/app.js
+++ webpack_example/src/app.js
@@ -2,11 +2,15 @@ import React, {useState} from 'react'
 
 const App = () => {
     const [counter, setCounter] = useState(0)
-
+    const [values, setValues] = useState([])
+    const handleClick = () => {
+        setCounter(counter + 1)
+        setValues(values.concat(counter))
+    }
     return (
         <div className={"container"}>
             hello webpack {counter}
-            <button onClick={() => setCounter(counter + 1)}>plus</button>
+            <button onClick={handleClick}>plus</button>
         </div>
     )
 }
