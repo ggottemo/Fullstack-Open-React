a4a21065cb3a71fab1329e861284da2224223ca1
ggottemo <ggottemo717@gmail.com>
2022-11-12 22:47:57 -0500

    8.7 editAuthor

--- GraphQLBackend/index.js
+++ GraphQLBackend/index.js
@@ -118,6 +118,10 @@ const typeDefs = gql`
           published: Int!
             genres: [String!]!
       ) : Book
+        editAuthor(
+            name: String!
+            setBornTo: Int!
+        ) : Author
       
   
   }
@@ -152,9 +156,20 @@ const resolvers = {
             books = books.concat(book)
             return book
 
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find( author => author.name === args.name)
+            if (!author) {
+                return null
+            }
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+            return updatedAuthor
+
+            }
         }
     }
-}
+
 // The ApolloServer constructor
 const server = new ApolloServer({
     typeDefs,
